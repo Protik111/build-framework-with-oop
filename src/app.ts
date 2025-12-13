7e506057c1a07244033a9fcb49f10ac99f099028
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-12-13 18:35:13 +0600

    feat: docker env

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,60 @@
-import express from 'express';
-import authRouter from '@/routers/auth.routes';
-import genreRouter from '@/routers/genre.routes';
-import publisherRouter from '@/routers/publishers';
-import userRouter from '@/routers/user.routes';
-import bookRouter from '@/routers/book.routes';
-import userRouterV2 from '@/examples/user.router';
-import bookRouterV2 from '@/examples/book.router';
+import express, { NextFunction, Request, Response } from "express";
+import authRouter from "@/routers/auth.routes";
+import genreRouter from "@/routers/genre.routes";
+import publisherRouter from "@/routers/publishers";
+import userRouter from "@/routers/user.routes";
+import bookRouter from "@/routers/book.routes";
+import userRouterV2 from "@/examples/user.router";
+import bookRouterV2 from "@/examples/book.router";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-// Register routes
-app.use('/api/auth', authRouter);
-app.use('/api/genres', genreRouter);
-app.use('/api/publishers', publisherRouter);
-app.use('/api/users', userRouter);
-app.use('/api/books', bookRouter);
+//health routes
+app.use("/health", (_req, res) => {
+  res.status(200).json({
+    health: "Ok",
+  });
+});
 
-app.use('/api/v2/users', userRouterV2);
-app.use('/api/v2/books', bookRouterV2);
+// Register routes
+app.use("/api/auth", authRouter);
+app.use("/api/genres", genreRouter);
+app.use("/api/publishers", publisherRouter);
+app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
+
+app.use("/api/v2/users", userRouterV2);
+app.use("/api/v2/books", bookRouterV2);
 
 // Error handling middleware
 app.use(
-	(
-		err: Error,
-		req: express.Request,
-		res: express.Response,
-		next: express.NextFunction
-	) => {
-		console.error(err.stack);
-		res.status(500).json({ message: 'Something went wrong!' });
-	}
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Something went wrong!" });
+  }
 );
+
+//not found hanlder
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API not found",
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
