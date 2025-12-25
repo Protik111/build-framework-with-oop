8a1a7f2cec749c4bec9a8fc648aa98ff7fca15c1
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-12-26 00:03:31 +0600

    docs: comments added

--- src/lib/core/IBaseRepository.ts
+++ src/lib/core/IBaseRepository.ts
@@ -1,46 +1,47 @@
-import { SQLWrapper } from 'drizzle-orm';
-import { PgColumn, PgTable } from 'drizzle-orm/pg-core';
+import { SQLWrapper } from "drizzle-orm";
+import { PgColumn, PgTable } from "drizzle-orm/pg-core";
 
 export type ID = string | number;
 
-export type OrderDirection = 'asc' | 'desc';
+export type OrderDirection = "asc" | "desc";
 
 export type FindOptionsSQL = {
-	where?: SQLWrapper;
-	limit?: number;
-	offset?: number;
-	orderBy?: {
-		column: PgColumn;
-		direction: OrderDirection;
-	}[];
+  where?: SQLWrapper;
+  limit?: number;
+  offset?: number;
+  orderBy?: {
+    column: PgColumn;
+    direction: OrderDirection;
+  }[];
 };
 
+//generic base repository interface
 export interface IBaseRepository<TTable extends PgTable & { id: SQLWrapper }> {
-	// Queries
-	findAll(options?: FindOptionsSQL): Promise<TTable['$inferSelect'][]>;
-	findById(id: ID): Promise<TTable['$inferSelect'] | null>;
-	findOne(where: SQLWrapper): Promise<TTable['$inferSelect'] | null>;
-	findAndCount(
-		options?: FindOptionsSQL
-	): Promise<[TTable['$inferSelect'][], number]>;
+  // Queries
+  findAll(options?: FindOptionsSQL): Promise<TTable["$inferSelect"][]>;
+  findById(id: ID): Promise<TTable["$inferSelect"] | null>;
+  findOne(where: SQLWrapper): Promise<TTable["$inferSelect"] | null>;
+  findAndCount(
+    options?: FindOptionsSQL
+  ): Promise<[TTable["$inferSelect"][], number]>;
 
-	count(where?: SQLWrapper): Promise<number>;
-	checkExists(where: SQLWrapper): Promise<boolean>;
+  count(where?: SQLWrapper): Promise<number>;
+  checkExists(where: SQLWrapper): Promise<boolean>;
 
-	// Mutations: Create
-	create(data: TTable['$inferInsert']): Promise<TTable['$inferSelect']>;
-	createMany(data: TTable['$inferInsert'][]): Promise<TTable['$inferSelect'][]>;
+  // Mutations: Create
+  create(data: TTable["$inferInsert"]): Promise<TTable["$inferSelect"]>;
+  createMany(data: TTable["$inferInsert"][]): Promise<TTable["$inferSelect"][]>;
 
-	// Mutations: Update
-	update(
-		id: ID,
-		data: Partial<TTable['$inferInsert']>
-	): Promise<TTable['$inferSelect'] | null>;
-	updateMany(
-		data: (Partial<TTable['$inferInsert']> & { id: ID })[]
-	): Promise<TTable['$inferSelect'][]>;
+  // Mutations: Update
+  update(
+    id: ID,
+    data: Partial<TTable["$inferInsert"]>
+  ): Promise<TTable["$inferSelect"] | null>;
+  updateMany(
+    data: (Partial<TTable["$inferInsert"]> & { id: ID })[]
+  ): Promise<TTable["$inferSelect"][]>;
 
-	// Mutations: Delete
-	delete(id: ID): Promise<void>;
-	deleteMany(ids: ID[]): Promise<void>;
+  // Mutations: Delete
+  delete(id: ID): Promise<void>;
+  deleteMany(ids: ID[]): Promise<void>;
 }
